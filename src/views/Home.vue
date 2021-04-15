2b932ecc9353b572eeda8797bad2e7314cfd8d1d
Kamil Jasiński <k.jasinski@ideo.pl>
2021-04-15 16:11:49 +0200

    Search bar w Home zawsze będzie zmieniał stronę
    - SearchBar w widoku Home zawsze będzie zmieniał stronę na widok UserRepos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,28 @@
 <template>
-  <div class="home">
-    <SearchBar />
-   
-  </div>
+   <div class="home">
+      <SearchBar :changeRoute="true" />
+   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import SearchBar from "@/components/SearchBar.vue"; // @ is an alias to /src
 
-
 @Component({
-  components: {
-    SearchBar,
-  
-  },
+   components: {
+      SearchBar,
+   },
 })
-export default class Home extends Vue {
-}
+export default class Home extends Vue {}
 </script>
 
 <style lang="scss">
- .home {
-    display: flex;
-    min-height: 800px;
-    width: 800px;
-    justify-content: center;
-    align-items: center;
-    // background-color: #fff;
-    margin: auto auto;
-  }
+.home {
+   display: flex;
+   min-height: 800px;
+   width: 800px;
+   justify-content: center;
+   align-items: center;
+   margin: auto auto;
+}
 </style>
