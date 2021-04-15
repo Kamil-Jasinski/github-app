f3cec09f1547a5d31a92e221e3ac68d87ded619b
Kamil Jasiński <k.jasinski@ideo.pl>
2021-04-15 16:00:52 +0200

    Dodano reużywalny komponent - kontener
    - Dodano możliwość zmiany wyglądu komponentu poprzez przekazywane propy

--- src/components/core/TheContainer.vue
+++ src/components/core/TheContainer.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="container">
-    <slot></slot>
-  </div>
+   <div
+      class="core-container"
+      :style="{
+         width: width,
+         minWidth: minWidth,
+         margin: margin,
+         backgroundColor: bgColor,
+         borderColor: borderColor,
+         borderRadius: borderRadius,
+         boxShadow: boxShadow,
+         background: bg,
+      }"
+   >
+      <slot></slot>
+   </div>
 </template>
 
-<style lang="scss">
-$main-app-color-light: #2d3238;
-$main-app-color-dark: #24292e;
-$main-app-shadow: #1b1e22;
+<script>
+import { Component, Vue, Prop } from "vue-property-decorator";
 
-.container {
-  width: 75%;
-  height: min-content;
-  display: flex;
-  flex-direction: column;
-  background-color: $main-app-color-light;
-  border-radius: 10px;
-  border: 4px solid $main-app-color-dark;
-  padding: 20px;
-  -webkit-box-shadow: 0px 3px 30px -1px $main-app-shadow;
-  box-shadow: 0px 3px 30px -1px $main-app-shadow;
+@Component
+export default class TheContainer extends Vue {
+   @Prop({ default: "auto" }) width;
+   @Prop({ default: "auto" }) minWidth;
+   @Prop({ default: "10px" }) margin;
+   @Prop({ default: null }) bgColor;
+   @Prop({ default: null }) borderColor;
+   @Prop({ default: "15px" }) borderRadius;
+   @Prop({ default: null }) boxShadow;
+   @Prop({ default: null }) bg;
+}
+</script>
+<style lang="scss" >
+.core-container {
+   height: min-content;
+
+   background-color: $main-app-color-light;
+   border-radius: 10px;
+   border: 4px solid $main-app-color-dark;
+   padding: 20px;
+   -webkit-box-shadow: 0px 3px 30px -1px $main-app-shadow-color;
+   box-shadow: 0px 3px 30px -1px $main-app-shadow-color;
 }
 </style>
