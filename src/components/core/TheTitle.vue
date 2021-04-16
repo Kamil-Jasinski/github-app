71086ef18fcadd3d280b89876be0177f9e4a651e
Kamil Jasiński <k.jasinski@ideo.pl>
2021-04-16 15:53:36 +0200

    Zmieniono max szerokość tytułu na 35 znaków

--- src/components/core/TheTitle.vue
+++ src/components/core/TheTitle.vue
@@ -1,62 +1,64 @@
 <template>
-  <div class="core-title-container" :style="{width: width, margin: margin, backgroundColor: bgColor, borderColor: borderColor}">
-    
-    <h2 class="core-title">
-        <slot></slot>
-    </h2>
-  </div>
+   <div
+      class="core-title-container"
+      :style="{
+         width: width,
+         margin: margin,
+         backgroundColor: bgColor,
+         borderColor: borderColor,
+      }"
+   >
+      <h2 class="core-title">
+         <slot></slot>
+      </h2>
+   </div>
 </template>
 
 <script>
 import { Component, Vue, Prop } from "vue-property-decorator";
 
-
 @Component
 export default class TheTitle extends Vue {
-  @Prop({default: 'auto'}) width;
-  @Prop({default: '10px'}) margin;
-  @Prop({default: null}) bgColor;
-  @Prop({default: null}) borderColor;
-
+   @Prop({ default: "auto" }) width;
+   @Prop({ default: "10px" }) margin;
+   @Prop({ default: null }) bgColor;
+   @Prop({ default: null }) borderColor;
 }
 </script>
 <style lang="scss">
-
-
 .core-title-container {
-
-padding: 40px;
-    .core-title {
-        margin: auto;
-        position: relative;
-        max-width: max-content;
-        color: $secondary-app-color-light;
-        text-transform: capitalize;
-        letter-spacing: .2rem;
-        &::before{
-            content: '';
-            display: block;
-            width: 70px;
-            height: 0.5px;
-            background-color: $third-app-color-light;
-            position: absolute;
-            top: 50%;
-            left: -90px;
-            transform: translateY(-50%);
-            
-        }
-         &::after{
-            content: '';
-            display: block;
-            width: 70px;
-            height: 0.5px;
-            background-color: $third-app-color-light;
-            position: absolute;
-            top: 50%;
-            right: -90px;
-            transform: translateY(-50%);
-            
-        }
-    }
+   padding: 40px;
+   .core-title {
+      margin: auto;
+      position: relative;
+      max-width: max-content;
+      color: $secondary-app-color-light;
+      text-transform: capitalize;
+      letter-spacing: 0.2rem;
+      max-width: 35ch;
+      text-align: center;
+      &::before {
+         content: "";
+         display: block;
+         width: 70px;
+         height: 0.5px;
+         background-color: $third-app-color-light;
+         position: absolute;
+         top: 50%;
+         left: -90px;
+         transform: translateY(-50%);
+      }
+      &::after {
+         content: "";
+         display: block;
+         width: 70px;
+         height: 0.5px;
+         background-color: $third-app-color-light;
+         position: absolute;
+         top: 50%;
+         right: -90px;
+         transform: translateY(-50%);
+      }
+   }
 }
 </style>
