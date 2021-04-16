9fd7ea6a06c7d4fed6a1e7faf32971e7a69a6f68
Kamil Jasiński <k.jasinski@ideo.pl>
2021-04-16 15:47:11 +0200

    Zmiana struktury, styli modala
    - dzięki zmianie struktury można wyłączyć okno modalne za pomocą kliknięcia w maskę

--- src/components/core/TheModal.vue
+++ src/components/core/TheModal.vue
@@ -1,73 +1,102 @@
 <template>
    <transition name="modal">
-      <div class="modal-mask">
-         <div class="modal-wrapper">
-            <div class="modal-container">
-               <div class="modal-header">
+      <section id="modal">
+         <div class="modal-mask" @click="$emit('close')"></div>
+
+         <div class="modal-container">
+            <div class="modal-header">
+               <h3>
                   <slot name="header">
                      <!-- default header -->
                   </slot>
-               </div>
+               </h3>
+            </div>
 
-               <div class="modal-body">
-                  <slot name="body">
-                     <!-- default body  -->
-                  </slot>
-               </div>
+            <div class="modal-body">
+               <slot name="body">
+                  <!-- default body  -->
+               </slot>
+            </div>
 
-               <div class="modal-footer">
-                  <slot name="footer">
-                     <!-- default footer -->
-                     <button
-                        class="modal-default-button"
-                        @click="$emit('close')"
-                     >
-                        OK
-                     </button>
-                  </slot>
-               </div>
+            <div class="modal-footer">
+               <slot name="footer">
+                  <!-- default footer -->
+                  <button class="modal-default-button" @click="$emit('close')">
+                     OK
+                  </button>
+               </slot>
             </div>
          </div>
-      </div>
+      </section>
    </transition>
 </template>
 
 <style lang="scss">
-.modal-mask {
+#modal {
    position: fixed;
    z-index: 9998;
-   top: 0;
-   left: 0;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
    width: 100%;
    height: 100%;
    background-color: rgba(0, 0, 0, 0.5);
-   display: table;
+   display: flex;
+   justify-content: center;
+   align-items: center;
    transition: opacity 0.3s ease;
 }
-
-.modal-wrapper {
-   display: table-cell;
-   vertical-align: middle;
+.modal-mask {
+   position: absolute;
+   top: 0;
+   left: 0;
+   bottom: 0;
+   right: 0;
+   background-color: rgba(0, 0, 0, 0.2);
 }
 
 .modal-container {
-   width: 300px;
+   position: relative;
+   z-index: 9999;
+
+   min-height: max-content;
+   max-height: 75vh;
+
+   width: min-content;
+   min-width: 300px;
+   // max-width: 800px;
+
    margin: 0px auto;
    padding: 20px 30px;
    background-color: $main-app-color-dark;
    border-radius: 2px;
    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
    transition: all 0.3s ease;
-   font-family: Helvetica, Arial, sans-serif;
+   overflow-y: auto;
+   overflow-x: hidden;
+
+   &::-webkit-scrollbar {
+      width: 6px;
+      height: 5px;
+   }
+   &::-webkit-scrollbar-track {
+      background-color: #ffffff25;
+   }
+   &::-webkit-scrollbar-thumb {
+      background-color: $secondary-app-color-dark;
+      border-radius: 8px;
+   }
 }
 
 .modal-header h3 {
    margin-top: 0;
-   color: #42b983;
+   color: $third-app-color-dark;
+   font-size: 2rem;
 }
 
 .modal-body {
    margin: 20px 0;
+   width: 100%;
 }
 
 .modal-footer {
