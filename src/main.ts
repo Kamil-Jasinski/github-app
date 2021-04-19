7ef89fa77e76f0503be6870fe63ad5bab3bda91e
Kamil Jasiński <k.jasinski@ideo.pl>
2021-04-19 13:05:21 +0200

    Dodanie ikon z fontawesome

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,47 @@ import store from "./store";
 import axios from 'axios';
 import VueAxios from 'vue-axios'
 import VueLuxon from "vue-luxon";
+import TheLoader from '@/components/core/TheLoader.vue';
+Vue.component('TheLoader', TheLoader)
 
 Vue.use(VueAxios, axios)
 Vue.use(VueLuxon);
 
+
+
+
+
+
+// FONT AWESOME
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import {
+  faUser,
+  faUserClock,
+  faMapMarkerAlt,
+  faFileWord,
+  faAt,
+  faArrowLeft,
+  faHome
+  
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  
+  faUser,
+  faUserClock,
+  faMapMarkerAlt,
+  faFileWord,
+  faTwitter,
+  faAt,
+  faArrowLeft,
+  faHome
+
+ 
+);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 new Vue({
