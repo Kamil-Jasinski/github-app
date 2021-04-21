193258732ba60132522ace240be601ed759f3067
Kamil Jasiński <k.jasinski@ideo.pl>
2021-04-21 16:02:09 +0200

    Dodanie portal-vue
    portal-vue umożliwia teleportowanie komponentu do ustalonego targetu w DOM

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,20 @@ import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import VueLuxon from "vue-luxon";
-import TheLoader from "@/components/core/TheLoader.vue";
+import PortalVue from "portal-vue";
+
 import TheNav from "@/components/core/TheNav.vue";
+import TheLoader from "@/components/core/TheLoader.vue";
+import TheModal from "@/components/core/TheModal.vue";
+import TheContainer from "@/components/core/TheContainer.vue";
 Vue.component("TheLoader", TheLoader);
 Vue.component("TheNav", TheNav);
+Vue.component("TheModal", TheModal);
+Vue.component("TheContainer", TheContainer);
 
 Vue.use(VueAxios, axios);
 Vue.use(VueLuxon);
+Vue.use(PortalVue);
 
 // FONT AWESOME
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
